71d005d4eae2755ca0115b85f313fdf74909bacf
summer18021 <summer18021@gmail.com>
2017-03-10 16:24:35 -0800

    Update todolist_js.js

--- todolist_js.js
+++ todolist_js.js
@@ -4,16 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>To-do list - Javascript</title>
+  <title>To do list - Javascript</title>
 
   <style media="screen">
-    ul{
+    div{
       list-style: none;
       padding:10;
       margin:10;
       width:40%;
       float:left;
-      border:1px solid #333;
       background:#eee;
     }
   </style>
@@ -21,7 +20,7 @@
 </head>
 <body>
 
-<h1>To do list</h1>
+<h1>To do list - javascript</h1>
 
 <input type="text" id="input"/>
 <button id="btn">ADD</button>
@@ -29,68 +28,76 @@
 <hr/>
 
 <!-- to do -->
+<div>
+<p>things to do</p>
 <ul id="todo">
 </ul>
+</div>
 
 <!-- done -->
+<div>
+<p>things being done</p>
 <ul id="done">
 </ul>
+</div>
 
 <script type="text/javascript">
-// self invoke function
-  ( function () {
-      var input = document.getElementById('input');
-      var btn = document.getElementById('btn');
-      var lists = {
-          todo:document.getElementById('todo'),
-          done:document.getElementById('done')
-      };
+// self invoking function
+  (function(){
+    var input = document.getElementById('input');
+    var btn = document.getElementById('btn');
+    var lists = {
+      todo: document.getElementById('todo'),
+      done: document.getElementById('done')
+    };
 
-      var makeTaskHtml = function(str,onCheck) {
-          var el = document.createElement('li');
-          var checkbox = document.createElement('input');
-          var label = document.createElement('span');
+    var makeTaskHtml = function(str,onCheck) {
+      var el = document.createElement('li');
+      var checkbox = document.createElement('input');
+      var label = document.createElement('span');
 
-          checkbox.type = 'checkbox';
-          checkbox.addEventListener('click',onCheck);
-          label.textContent = str;
+      checkbox.type = 'checkbox';
+      checkbox.addEventListener('click', onCheck);
+      label.textContent = str;
 
-          el.appendChild(checkbox);
-          el.appendChild(label);
+      el.appendChild(checkbox);
+      el.appendChild(label);
 
-          return el;
-      };
+      return el;
+    };
 
-      var addTask = function(task) {
-          lists.todo.appendChild(task);
-      };
+    var addTask = function(task) {
+      lists.todo.appendChild(task);
+    };
 
-      var onCheck = function(event) {
-          var task = event.target.parentElement;
-          var list = task.parentElement.id;
-          lists[list === 'done'?'todo':'done'].appendChild(task);
-          this.checked = false;
-          input.focus();
-      };
-
-      var onInput = function() {
-          var str = input.value.trim();
-          if (str.length > 0) {
-              addTask(makeTaskHtml(str, onCheck));
-              input.value = '';
-              input.focus();
-          }
-      };
-
-      btn.addEventListener('click',onInput);
-      input.addEventListener('keyup', function(event) {
-          var code = event.keyCode;
-          if (code === 13) {
-              onInput();
-          }
-      });
+    var onCheck = function(event) {
+      var task = event.target.parentElement;
+      var list = task.parentElement.id;
+      lists[list === 'done'? 'todo': 'done'].appendChild(task);
+      this.checked = false;
       input.focus();
+    };
+
+    var onInput = function() {
+      var str = input.value.trim();
+      if (str.length > 0) {
+        addTask(makeTaskHtml(str, onCheck));
+        input.value = '';
+        input.focus();
+      }
+    };
+
+    btn.addEventListener('click', onInput);
+    input.addEventListener('keyup', function(event) {
+      var code = event.keyCode;
+      if (code === 13) {
+        onInput();
+      }
+    });
+    input.focus();
+
   }());
 </script>
 </body>
 </html>
+
